19647d1b549e93cc8eb22254fb4384afe6463b2f
pangedza <fatal47887@gmail.com>
2025-06-10 09:33:06 +0300

    Update welcome screen UI with new background and buttons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,11 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pangedza_delivery/main.dart';
 
 void main() {
-  testWidgets('Start page shows main buttons', (WidgetTester tester) async {
+  testWidgets('Start page shows new action buttons', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.text('Вход'), findsOneWidget);
-    expect(find.text('Регистрация'), findsOneWidget);
+    expect(find.text('Продолжить'), findsOneWidget);
     expect(find.text('Быстрый заказ'), findsOneWidget);
   });
 }
