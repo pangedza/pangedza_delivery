f379809ef246313534979bd5a86321e407c4fce0
pangedza <fatal47887@gmail.com>
2025-06-15 20:53:37 +0300

    Fix ApiService to fetch stop list

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 9261addadbfce796ef6cb1536727ddf096153b90
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -10,26 +6,11 @@ class ApiService {
 
   static Future<List<dynamic>> fetchStopList() async {
     final response = await http.get(Uri.parse('$baseUrl/stoplist'));
+
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
       throw Exception('Failed to load stop list');
     }
   }
-<<<<<<< HEAD
-
-  static Future<void> addStopItem(String name) async {
-    await http.post(
-      Uri.parse('$baseUrl/stoplist'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({"id": DateTime.now().millisecondsSinceEpoch, "name": name}),
-    );
-  }
-
-  static Future<void> removeStopItem(int id) async {
-    await http.delete(Uri.parse('$baseUrl/stoplist/$id'));
-  }
-=======
->>>>>>> 9261addadbfce796ef6cb1536727ddf096153b90
 }
-
