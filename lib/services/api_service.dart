d066f16a10f65b3d0af484df0d8bfe2b9de5202a
pangedza <fatal47887@gmail.com>
2025-06-15 19:58:50 +0300

    Add API service and integrate stop list

---
+++ lib/services/api_service.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const baseUrl = 'http://localhost:3000/api';
+
+  static Future<List<dynamic>> fetchStopList() async {
+    final response = await http.get(Uri.parse('$baseUrl/stoplist'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load stop list');
+    }
+  }
+}
