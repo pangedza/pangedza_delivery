ee8584b370eafbaf7bf4bdcbef9b164285d3c346
pangedza <fatal47887@gmail.com>
2025-06-16 19:47:55 +0300

    Add phone mask and UI style for login and register

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import '../services/users_service.dart';
 import '../models/profile_model.dart';
 
@@ -31,23 +32,54 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final _phoneMask = MaskTextInputFormatter(
+      mask: '+7 (###) ###-##-##',
+      filter: {"#": RegExp(r'[0-9]')},
+    );
+
     return Scaffold(
       backgroundColor: Color(0xFFF9F9F9),
       appBar: AppBar(title: Text("Регистрация")),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 16),
             TextField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: "Имя")),
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: "Ваше имя",
+                hintText: "Имя",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              ),
+            ),
+            SizedBox(height: 16),
             TextField(
-                controller: _phoneController,
-                decoration: InputDecoration(labelText: "Телефон")),
+              controller: _phoneController,
+              inputFormatters: [_phoneMask],
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: "Ваш номер телефона",
+                hintText: "+7 (900) 000-00-00",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              ),
+            ),
+            SizedBox(height: 16),
             TextField(
-                controller: _pinController,
-                decoration: InputDecoration(labelText: "4-значный PIN")),
-            SizedBox(height: 20),
+              controller: _pinController,
+              keyboardType: TextInputType.number,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Придумайте PIN",
+                hintText: "4 цифры",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              ),
+            ),
+            SizedBox(height: 24),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.red,
