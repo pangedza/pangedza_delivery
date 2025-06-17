22afd6d2d85d7f346ac7687a203299abd4d84c5c
pangedza <fatal47887@gmail.com>
2025-06-17 19:26:33 +0300

    Make dish card image rectangular

--- lib/widgets/dish_card.dart
+++ lib/widgets/dish_card.dart
@@ -59,16 +59,16 @@ class DishCard extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: AspectRatio(
-                          aspectRatio: 4 / 3,
+                      Container(
+                        height: 100,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
                           child: Image.network(
                             dish.imageUrl.isNotEmpty
                                 ? dish.imageUrl
                                 : 'https://via.placeholder.com/512x300.png?text=%D0%91%D0%BB%D1%8E%D0%B4%D0%BE',
                             fit: BoxFit.cover,
-                            width: double.infinity,
                             errorBuilder: (_, __, ___) => Container(
                               color: Colors.grey[200],
                               alignment: Alignment.center,
