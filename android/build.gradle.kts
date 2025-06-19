f7df775b579f46e95238548143a0260b5ed783d4
pangedza <fatal47887@gmail.com>
2025-06-19 16:53:52 +0300

    Update Android build scripts for Kotlin DSL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,41 +1,22 @@
-plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.2.1"
+        id("org.jetbrains.kotlin.android") version "1.9.0"
+    }
 }
 
-buildscript {
-    ext.kotlin_version = '1.9.0' // ✅ обязательно!
-    ...
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-    }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-dependencies {
-    classpath("com.android.tools.build:gradle:8.2.1")
-}
+rootProject.name = "pangedza_delivery"
+include(":app")
