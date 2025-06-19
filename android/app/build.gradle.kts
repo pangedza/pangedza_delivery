28d2fba93db1c5242bcc47139d47881d7df4615b
pangedza <fatal47887@gmail.com>
2025-06-19 16:37:13 +0300

    update gradle and supabase versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.pangedza_delivery"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 33
     ndkVersion = "27.0.12077973"
 
     compileOptions {
